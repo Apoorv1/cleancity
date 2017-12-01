57224c5649325b5b031cd155d8b81b722339fa71
Apoorv1 <apoorvtikalkar@gmail.com>
2017-12-01 10:03:40 +0530

    initial commit

---
+++ app/src/main/java/com/example/android/tabbedexample/tab1pending.java
@@ -0,0 +1,23 @@
+package com.example.android.tabbedexample;
+
+/**
+ * Created by apoorv on 30-11-2017.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class tab1pending extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1pending, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/android/tabbedexample/tab2actiontaken.java
@@ -0,0 +1,22 @@
+package com.example.android.tabbedexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by apoorv on 30-11-2017.
+ */
+
+public class tab2actiontaken extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2actiontaken, container, false);
+
+        return rootView;
+    }
+}
