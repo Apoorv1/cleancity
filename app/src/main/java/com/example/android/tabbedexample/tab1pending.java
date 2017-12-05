b69095b6eb75bb8b50cb44a4d897b9853669c09d
Apoorv1 <apoorvtikalkar@gmail.com>
2017-12-05 14:58:02 +0530

    setting,about page

--- app/src/main/java/com/example/android/tabbedexample/tab1pending.java
+++ app/src/main/java/com/example/android/tabbedexample/tab1pending.java
@@ -7,11 +7,14 @@ package com.example.android.tabbedexample;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class tab1pending extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
